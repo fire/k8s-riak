e19da84bd764ec57a6f29662194c384ee3857d6c
Jan-Philip Loos <maxdaten@gmail.com>
2015-10-27 01:39:58 +0100

    Squashed 'riak-bootstrapper/' changes from 6570c1e..487e194
    487e194 Update environemnt variables
    
    git-subtree-dir: riak-bootstrapper
    git-subtree-split: 487e19446b094fb4b42bf17219614e4ae6267ed3

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --install-ghc runghc --package turtle --package lens --package lens-aeson --package wreq --package envy --package string-conversions  --package errors --package bytestring --package http-client-tls
+-- stack --install-ghc runghc --package turtle --package lens --package lens-aeson --package wreq --package envy --package string-conversions  --package errors --package bytestring --package http-client-tls --package text
 
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -10,17 +10,20 @@ import           Control.Lens            hiding ((.=))
 import           Data.Aeson              as JSON
 import           Data.Aeson.Lens
 import           Data.Map                as Map
-import           Data.List               (isPrefixOf)
+import           Data.List               as L (isPrefixOf)
+import qualified Data.Text               as T
 import           Data.String.Conversions (cs)
 import           Data.ByteString         as BS hiding (isPrefixOf)
 import           Network.Wreq            as Http
 import           Network.HTTP.Client.TLS as TLS
 import           Network.Connection      as TLS
-import           System.Envy             as Env (FromEnv (fromEnv), decodeEnv,
+import           System.Envy             as Env (Var(..), FromEnv (fromEnv), env, decodeEnv,
                                                  envMaybe, (.!=))
 import           Turtle                  as Shell
 import           Control.Error
 import           Control.Error.Script
+
+import Debug.Trace
 
 data Endpoint = Endpoint { ip :: Text }
   deriving (Show,Eq)
@@ -32,19 +35,25 @@ instance FromJSON Endpoint where
 instance ToJSON Endpoint where
   toJSON (Endpoint ip) = JSON.object [ "ip" .= ip ]
 
+newtype KubernetesApi = KubernetesApi { getUrl :: Text }
+  deriving (Show, Read, Eq, Ord)
+
+instance Env.Var KubernetesApi where
+  toVar = cs . getUrl
+  fromVar = Just . KubernetesApi . (<>) "https://" . cs
+
 data Environment = Environment
-  { kubernetesService    :: Text
+  { kubernetesService    :: KubernetesApi
   , riakNamespace        :: Text
   , riakDiscoveryService :: Text
   } deriving (Show,Eq)
 
 instance FromEnv Environment where
-  fromEnv = do
-    domain <- envMaybe "DNS_DOMAIN" Env..!= "cluster.local"
+  fromEnv =
     Environment
-      <$> envMaybe "KUBERNETES_MASTER" Env..!= ("https://kubernetes.default.svc." <> domain)
-      <*> envMaybe "RIAK_NAMESPACE" Env..!= "default"
-      <*> envMaybe "RIAK_DISCOVERY_SERVICE" Env..!= "riak-discovery"
+    <$> Env.envMaybe "KUBERNETES_PORT_443_TCP_ADDR" Env..!= KubernetesApi "https://kubernetes"
+    <*> envMaybe "RIAK_NAMESPACE" Env..!= "default"
+    <*> envMaybe "RIAK_DISCOVERY_SERVICE" Env..!= "riak-discovery"
 
 main :: IO ()
 main = do
@@ -54,7 +63,7 @@ main = do
           then secureConnectionSettings
           else return defaults
 
-  -- Query k8s Api
+  -- Query service endpoints via http k8s Api
   response <- Http.getWith opts endpoints
 
   -- Extract first endpoint of the service
@@ -62,7 +71,7 @@ main = do
                     . key "subsets"
                     ... key "addresses"
                     . nth 0 . _JSON
-  -- riakAdmin []
+  riakAdmin ["status"]
   print (mEndpoint :: Maybe Endpoint)
   -- print (mEndpoints :: Maybe [Endpoint])
 
@@ -72,16 +81,17 @@ secureConnectionSettings :: IO Options
 secureConnectionSettings = do
   -- cert  <- BS.readFile "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
   -- HTTPS read bearer token http://stackoverflow.com/a/30739416
-  token <- BS.readFile "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+  token <- BS.readFile "/var/run/secrets/kubernetes.io/serviceaccount/token"
   let tlsSettings = TLSSettingsSimple True False False -- FIXME: mitm
-  return $ defaults & auth ?~ oauth2Bearer token
-                    & manager .~ Left (mkManagerSettings tlsSettings Nothing)
+  return $ defaults
+            & auth ?~ oauth2Bearer token
+            & manager .~ Left (mkManagerSettings tlsSettings Nothing)
 
 endpointQueryUrl :: Environment -> Text
 endpointQueryUrl Environment{..} =
   Shell.format
   (s%"/api/v1/namespaces/"%s%"/endpoints/"%s)
-  kubernetesService
+  (getUrl kubernetesService)
   riakNamespace
   riakDiscoveryService
 
