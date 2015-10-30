b3e30ff2f20448ee320363ae14d8e54fb0b46062
Jan-Philip Loos <maxdaten@gmail.com>
2015-10-30 08:30:29 +0100

    Simple test script to run bootstrapped on k8s pods

---
+++ exec-bootstrap.sh
@@ -0,0 +1,5 @@
+
+STACK="stack --stack-yaml /home/jloos/stack.yaml"
+
+# kubectl exec $1 -- bash -x -c "${STACK} setup && ${STACK} build && ${STACK} install && ${STACK} exec k8s-riak-bootstrapper"
+kubectl exec $1 -- bash -x -c "/home/jloos/src/Main.hs"
